e9bc3148f8eb9b1c81a1e4f65e3211481a0224b1
Alexander Aust <mail@alexander-aust.de>
2024-03-08 11:43:44 +0100

    Add swagger-ui in backend

--- backend/cmd/foodiee_backend/main.go
+++ backend/cmd/foodiee_backend/main.go
@@ -20,6 +20,11 @@ func main() {
 	}
 
 	api.RegisterHandlers(router, server)
+	router.Static("/swagger-ui", "./swagger-ui")
+	router.GET("/api.yaml", func(c *gin.Context) {
+		c.File("./docs/api.yaml")
+	})
+
 	err := router.Run(":8080")
 	if err != nil {
 		log.Fatal("Could not start server: ", err)
